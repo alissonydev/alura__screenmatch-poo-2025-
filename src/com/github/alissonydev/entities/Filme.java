f7fb790650864436a0f33aafe527aff8761f9cb6
alissonydev <alissonydev@outlook.com>
2025-10-14 13:53:51 -0300

    Classe Filme

---
+++ src/com/github/alissonydev/entities/Filme.java
@@ -0,0 +1,14 @@
+package com.github.alissonydev.entities;
+
+public class Filme {
+
+    String nome;
+    int anoDeLancamento;
+    String genero;
+    double nota;
+    boolean incluidoNoPlano;
+    double avaliacao;
+    int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+}
