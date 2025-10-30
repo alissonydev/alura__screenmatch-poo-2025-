1f0b41ec60b5f38acb53e9644fcb6f08d27d5641
alissonydev <alissonydev@outlook.com>
2025-10-30 12:15:28 -0300

    Polimorfismo, evitando codigo repetido

--- src/com/github/alissonydev/utils/CalculadoraDeTempo.java
+++ src/com/github/alissonydev/utils/CalculadoraDeTempo.java
@@ -2,6 +2,7 @@ package com.github.alissonydev.utils;
 
 import com.github.alissonydev.entities.Filme;
 import com.github.alissonydev.entities.Serie;
+import com.github.alissonydev.entities.Titulo;
 
 public class CalculadoraDeTempo {
 
@@ -11,11 +12,15 @@ public class CalculadoraDeTempo {
         return tempoTotal;
     }
 
-    public void inclui(Filme f) {
-        tempoTotal += f.getDuracaoEmMinutos();
-    }
+//    public void inclui(Filme f) {
+//        tempoTotal += f.getDuracaoEmMinutos();
+//    }
+//
+//    public void inclui(Serie s) {
+//        tempoTotal += s.getDuracaoEmMinutos();
+//    }
 
-    public void inclui(Serie s) {
-        tempoTotal += s.getDuracaoEmMinutos();
+    public void inclui(Titulo t) {
+        tempoTotal += t.getDuracaoEmMinutos();
     }
 }
